2da3a682ad9602eb0db9db38c19325c2aadd1ef6
Anton <ministrov2018@gmail.com>
2026-06-16 21:46:46 +0300

    feat(expences): write a code realisation for expences task of homework

--- homeworks/2_operations/expences.py
+++ homeworks/2_operations/expences.py
@@ -3,6 +3,14 @@
     Задача: Ввести три траты: еда, транспорт, развлечения. Вывести общую сумму и среднее - файл expences.py
 """
 
+MAX_VALUE = 3
+
 food = float(input("Еда: "))
 transport = float(input("Транспорт: "))
 entertainment = float(input("Развлечения: "))
+
+overall_sum = food + transport + entertainment
+average = overall_sum // MAX_VALUE
+
+print(overall_sum)
+print(average)
