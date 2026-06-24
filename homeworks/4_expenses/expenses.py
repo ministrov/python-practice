09f8d938bb7caf4747017d3150e42310cd68dc77
Zhila <ministrov2018@gmail.com>
2026-06-24 13:35:43 +0300

    homework(expenses): add weekly expense variables and list with proper type annotation

--- homeworks/4_expenses/expenses.py
+++ homeworks/4_expenses/expenses.py
@@ -4,3 +4,14 @@
             Посчитать сумму, среднее, минимум и максимум.
             Сохранить в кортеже (минимум, максимум, сумма) и вывести его.
 """
+
+flat_rent = 750
+mobile_expense = 120
+food = 450
+clothes = 700
+fitness = 340
+console_game = 345
+going_out = 560
+
+expenses: list[int] = [flat_rent, mobile_expense,
+                       food, clothes, fitness, console_game, going_out]
