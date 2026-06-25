aa79c172c896424516c68a5c084ca662a338c38e
Zhila <ministrov2018@gmail.com>
2026-06-25 10:17:24 +0300

    homework(loops): clean up test code - rock-paper-scissors game complete

--- programms/game_rps.py
+++ programms/game_rps.py
@@ -51,17 +51,3 @@ elif user_score < computer_score:
     print("Компьютер победил в игре")
 else:
     print("Ничья")
-print(random)
-# numbers: list[int] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# random_num = random.choice(numbers)
-
-# evens: list[int] = []
-
-# for num in numbers:
-#     if num % 2 == 0:
-#         evens.append(num)
-#     else:
-#         evens = []
-
-# print(random_num)
-# print(evens)
