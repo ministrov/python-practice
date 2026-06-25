573a5eaed9916dcc7e8715ad82aaecde25269610
Zhila <ministrov2018@gmail.com>
2026-06-25 13:05:23 +0300

    homework(loops): practice filtering even numbers with nested loops

--- programms/multiplication_table.py
+++ programms/multiplication_table.py
@@ -2,15 +2,27 @@
     Задача: с помощью цикла for создать таблицу умножения от 1 до 10
 """
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        print(f"{i*j:4}", end="")
-    print()
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print(f"{i*j:4}", end="")
+#     print()
 
-for c in "Hi":
-    print(c)
+# for c in "Hi":
+#     print(c)
 
-shopping_list = ["яблоки", "молоко", "хлеб", "сыр", "масло"]
+# shopping_list = ["яблоки", "молоко", "хлеб", "сыр", "масло"]
 
-for fruit in shopping_list:
-    print(f"{fruit} \n")
+# for fruit in shopping_list:
+#     print(f"{fruit} \n")
+
+numbers: list[int] = [3, 8, 5, 12, 7, 4, 11, 6]
+result: list[int] = []
+
+# Ваш код здесь
+for number in numbers:
+    if number % 2 == 1:
+        continue
+    else:
+        result.append(number)
+        for num in result:
+            print(num)
