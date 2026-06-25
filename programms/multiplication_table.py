3a74d9d4db3684f254eb04ee8acaf3cee42a5b11
Zhila <ministrov2018@gmail.com>
2026-06-25 12:48:56 +0300

    homework(loops): practice various loop patterns - strings and lists

--- programms/multiplication_table.py
+++ programms/multiplication_table.py
@@ -6,3 +6,11 @@ for i in range(1, 11):
     for j in range(1, 11):
         print(f"{i*j:4}", end="")
     print()
+
+for c in "Hi":
+    print(c)
+
+shopping_list = ["яблоки", "молоко", "хлеб", "сыр", "масло"]
+
+for fruit in shopping_list:
+    print(f"{fruit} \n")
