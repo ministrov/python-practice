b34d8a4e67e522220d511657f9c67c06c7de5a68
Zhila <ministrov2018@gmail.com>
2026-07-07 13:11:47 +0300

    feat(programms): add mock orders data for orders_stats.py

---
+++ programms/orders_stats.py
@@ -0,0 +1,15 @@
+""" Цель задачи — вычислить общее количество проданных товаров и общую сумму заказов за день, используя данные, хранящиеся в словарях.
+"""
+orders = [
+    {"id": 1, "user": "Anton", "items": [
+        {"name": "Laptop", "price": 1000},
+        {"name": "Mouse", "price": 50}
+    ]},
+    {"id": 2, "user": "Kate", "items": [
+        {"name": "Phone", "price": 700}
+    ]},
+    {"id": 3, "user": "Oleg", "items": [
+        {"name": "Monitor", "price": 300},
+        {"name": "Keyboard", "price": 100}
+    ]}
+]
