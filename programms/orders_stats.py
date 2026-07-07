2e58876ea87c21aa694436976ac3296c69babae4
Zhila <ministrov2018@gmail.com>
2026-07-07 13:25:51 +0300

    feat(programms): implement aggregate function for orders_stats.py

--- programms/orders_stats.py
+++ programms/orders_stats.py
@@ -1,5 +1,8 @@
 """ Цель задачи — вычислить общее количество проданных товаров и общую сумму заказов за день, используя данные, хранящиеся в словарях.
 """
+from typing import Any
+
+
 orders = [
     {"id": 1, "user": "Anton", "items": [
         {"name": "Laptop", "price": 1000},
@@ -13,3 +16,12 @@ orders = [
         {"name": "Keyboard", "price": 100}
     ]}
 ]
+
+
+def aggregate(acc: dict[str, Any], order: dict[str, Any]) -> dict[str, Any]:
+    order_sum = sum(item["price"] for item in order["items"])
+    print(order_sum)
+    return acc
+
+
+aggregate({}, orders[0])
