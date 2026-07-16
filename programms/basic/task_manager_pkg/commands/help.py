815a42f08ac4251f5baa01a0b7b5015975ef4f44
Zhila <ministrov2018@gmail.com>
2026-07-16 12:35:01 +0300

    feat(task_manager_pkg): scaffold list command module
    - Add list_command stub in commands/list.py
    - Update help text to mention list by=prio|due filtering

--- programms/basic/task_manager_pkg/commands/help.py
+++ programms/basic/task_manager_pkg/commands/help.py
@@ -5,7 +5,7 @@ def help_command():
     print(""" Команды: 
         add <title> [prio=low|med|high] [due=YYYY-MM-DD] [tags=a,b,c] - Добавить
             (title из нескольких слов оборачивай в кавычки: add "Купить молоко" prio=high)
-        list - Показать список
+        list [by=prio|due] - Показать список отфильтрованный по приоритеты либо дате
         done <id> - Выполнить
         edit <id> - [title=...] [prio=...] [due=YYYY-MM-DD]
         remove <id> - Удалить
---
+++ programms/basic/task_manager_pkg/commands/list.py
@@ -0,0 +1,7 @@
+""" Модуль вывода задач"""
+
+from task_manager_pkg.tasks.tasks import Task
+
+
+def list_command(tasks: list[Task], args: list[str]):
+    pass
